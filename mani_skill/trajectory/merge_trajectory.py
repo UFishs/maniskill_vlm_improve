9e1609fc3664bcb4a1fe202275702e3407fa1810
ruizihang <rzh22@mails.tsinghua.edu.cn>
2025-12-25 19:25:59 +0800

    feat: better merge script

--- mani_skill/trajectory/merge_trajectory.py
+++ mani_skill/trajectory/merge_trajectory.py
@@ -6,7 +6,7 @@ from mani_skill.utils.logging_utils import logger
 from mani_skill.utils.io_utils import dump_json, load_json
 
 
-def merge_trajectories(output_path: str, traj_paths: list, recompute_id: bool = True):
+def merge_trajectories(output_path: str, traj_paths: list, num_demos: list, recompute_id: bool = True):
     """
     Merges multiple JSON and H5 files into a single JSON and H5 file.
 
@@ -25,6 +25,9 @@ def merge_trajectories(output_path: str, traj_paths: list, recompute_id: bool = 
     Raises:
         AssertionError: If there is a conflict in the episode IDs when recompute_id is False.
     """
+    if num_demos is None:
+        num_demos = [None] * len(traj_paths)
+
     logger.info(f"Merging {output_path}")
 
     merged_h5_file = h5py.File(output_path, "w")
@@ -32,7 +35,7 @@ def merge_trajectories(output_path: str, traj_paths: list, recompute_id: bool = 
     merged_json_data = {"episodes": []}
     cnt = 0
 
-    for traj_path in traj_paths:
+    for file_idx, traj_path in enumerate(traj_paths):
         traj_path = str(traj_path)
         logger.info(f"Merging{traj_path}")
 
@@ -51,7 +54,14 @@ def merge_trajectories(output_path: str, traj_paths: list, recompute_id: bool = 
                         logger.warning(f"Conflict detected for key {key} in {traj_path}: {merged_json_data[key]} != {value}")
 
             # Merge episodes
-            for ep in json_data["episodes"]:
+
+            if num_demos[file_idx] is not None:
+                used_episodes = json_data["episodes"][:num_demos[file_idx]]
+            else:
+                used_episodes = json_data["episodes"]
+
+
+            for ep in used_episodes:
                 episode_id = ep["episode_id"]
                 traj_id = f"traj_{episode_id}"
 
@@ -82,15 +92,49 @@ def main():
     parser.add_argument("-p", "--pattern", type=str, default="trajectory.h5")
     args = parser.parse_args()
 
+
     traj_paths = []
-    for input_dir in args.input_dirs:
-        input_dir = Path(input_dir)
-        traj_paths.extend(sorted(input_dir.rglob(args.pattern)))
+    if args.input_dirs:
+        for input_dir in args.input_dirs:
+            input_dir = Path(input_dir)
+            traj_paths.extend(sorted(input_dir.rglob(args.pattern)))
+    
+    # traj_paths = [
+    #     '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/demos/StackThree-v1/motionplanning/base_traj_200.rgb.pd_joint_delta_pos.physx_cpu.h5',
+    #     '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/base_record/iter_1_base.h5'
+    # ]
+    # traj_paths = [
+    #     '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/demos/StackThree-v1/motionplanning/split_pd_joint_delta_pos/stage_1.h5',
+    #     '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/stage_1_record/iter_1_stage_1.h5',
+    # ]
+    # traj_paths = [
+    #     '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/demos/StackThree-v1/motionplanning/split_pd_joint_delta_pos/stage_2.h5',
+    #     '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/stage_2_record/iter_1_stage_2.h5',
+    # ]
+    # traj_paths = [
+    #     '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/demos/StackThree-v1/motionplanning/split_pd_joint_delta_pos/stage_3.h5',
+    #     '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/stage_3_record/iter_1_stage_3.h5',
+    # ]
+    traj_paths = [
+        '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/demos/StackThree-v1/motionplanning/split_pd_joint_delta_pos/stage_4.h5',
+        '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/stage_4_record/iter_1_stage_4.h5',
+    ]
+
+
+    num_demos = [
+        100,
+        None
+    ]
+    # args.output_path = '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/base_record/iter_1_base_merge.h5'
+    # args.output_path = '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/stage_1_record/iter_1_stage_1_merge.h5'
+    # args.output_path = '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/stage_2_record/iter_1_stage_2_merge.h5'
+    # args.output_path = '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/stage_3_record/iter_1_stage_3_merge.h5'
+    args.output_path = '/cephfs/gyshare/ruizihang/maniskill_vlm_improve/eval/system_collect/iter_1/stage_4_record/iter_1_stage_4_merge.h5'
 
     output_dir = Path(args.output_path).parent
     output_dir.mkdir(exist_ok=True, parents=True)
 
-    merge_trajectories(args.output_path, traj_paths)
+    merge_trajectories(args.output_path, traj_paths, num_demos)
 
 
 if __name__ == "__main__":
